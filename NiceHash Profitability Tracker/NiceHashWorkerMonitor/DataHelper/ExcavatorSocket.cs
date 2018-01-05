88cb12ac1af7c7ce4ef59a38bb12b04949fddab9
Jeremy Gulick <applegeek48@gmail.com>
2018-01-05 16:50:53 -0600

    Initial Commit for Worker Monitoring.

---
+++ NiceHash Profitability Tracker/NiceHashWorkerMonitor/DataHelper/ExcavatorSocket.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Globalization;
+using System.IO;
+using System.Net.Sockets;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NiceHashWorkerMonitor.DataHelper
+{
+	class ExcavatorSocket
+	{
+		private TcpClient _tcpclient;
+
+		private StreamReader _sReader;
+		private StreamWriter _sWriter;
+		public static List<string> lst_storeddata = new List<string>();
+
+		private Boolean _isConnected;
+		public dynamic Client(string ipAddress, int id, string method, string[] param)
+		{
+			//server ip
+			//String ipAddress = "192.168.43.15";
+			//port number
+			int portNum = 4000;
+			try
+			{
+				_tcpclient = new TcpClient();
+				_tcpclient.Connect(ipAddress, portNum);
+				return HandleCommunication(id, method, param);
+			}
+			catch (Exception ex)
+			{
+				return null;
+			}
+		}
+
+		public dynamic HandleCommunication(int id, string method, string[] param)
+		{
+			_sReader = new StreamReader(_tcpclient.GetStream(), Encoding.ASCII);
+			_sWriter = new StreamWriter(_tcpclient.GetStream(), Encoding.ASCII);
+			JObject requestData = new JObject(
+				new JProperty("id", id),
+				new JProperty("method", method),
+				new JProperty("params",GetJArrayFromStringArray(param)));
+			
+			_sWriter.WriteLine(requestData.ToString(Formatting.None));
+			string temp = requestData.ToString(Formatting.None);
+			_sWriter.Flush();
+
+			// receive data
+			String sDataIncomming = _sReader.ReadLine();
+			dynamic returnData = JsonConvert.DeserializeObject(sDataIncomming);
+			_sWriter.Close();
+			_tcpclient.Close();
+			return returnData;
+		}
+		public JArray GetJArrayFromStringArray(string[] param)
+		{
+			JArray array = new JArray();
+			foreach(string para in param)
+			{
+				array.Add(para);
+			}
+			return array;
+		}
+	}
+}
---
+++ NiceHash Profitability Tracker/NiceHashWorkerMonitor/NiceHashWorkerMonitor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NiceHashWorkerMonitor
+{
+	public partial class NiceHashWorkerMonitor : Form
+	{
+		public NiceHashWorkerMonitor()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			DataHelper.ExcavatorSocket sock = new DataHelper.ExcavatorSocket();
+			MessageBox.Show(sock.Client("127.0.0.1",1,"device.list",new string[0]).ToString());
+
+		}
+	}
+}
