08acac3e7798ea54732ba83fdf410d262a1f2543
jeremy.gulick <jeremy.gulick@peopleadmin.com>
2018-01-10 13:46:27 -0600

    NPC-2 Refactoring naming scheme to allow for zip extraction.

--- NiceHash Profitability Tracker/Viewer/Program.cs
+++ NiceHash Profitability Tracker/Viewer/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace NiceHashStatsViewer
+namespace Viewer
 {
 	static class Program
 	{
@@ -16,7 +16,7 @@ namespace NiceHashStatsViewer
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new NicehashStatsViewer());
+			Application.Run(new Viewer());
 		}
 	}
 }
