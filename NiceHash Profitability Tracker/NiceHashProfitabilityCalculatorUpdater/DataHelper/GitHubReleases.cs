8e9979ea449939491d52e78f06845d52d7e3b426
jeremy.gulick <jeremy.gulick@peopleadmin.com>
2018-01-10 11:54:34 -0600

    NPC-2 Adding Updater Appliction

---
+++ NiceHash Profitability Tracker/NiceHashProfitabilityCalculatorUpdater/DataHelper/GitHubReleases.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace NiceHashProfitabilityCalculatorUpdater.DataHelper
+{
+    public static class GitHubReleases
+    {
+        public static dynamic GetRepoLatestReleaseNumber()
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.github.com/repos/jgulick48/NiceHashProfitabilityCalculator/releases");
+            httpWebRequest.Method = "GET";
+            httpWebRequest.UserAgent = "NHPCApi";
+            httpWebRequest.KeepAlive = false;
+            try
+            {
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    dynamic returnData = JsonConvert.DeserializeObject(result);
+                    return returnData[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ NiceHash Profitability Tracker/NiceHashProfitabilityCalculatorUpdater/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace NiceHashProfitabilityCalculatorUpdater
+{
+    class Program
+    {
+        private static dynamic ReleaseInfo;
+        private static string updaterFolder;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking for running processes before update.");
+            CheckForRunningApps();
+            DeleteFilesInCurrentDirectory();
+            SaveInformationOnLatestRelease();
+            DownloadAppDownloader();
+            StartUpdateDownloader();
+        }
+        private static void CheckForRunningApps()
+        {
+            if(CheckIfAppRunningByName("NiceHashStatsViewer"))
+            {
+                Console.WriteLine("Error: Unable to update app due to NiceHashStatsViewer still running.");
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
+            else if (CheckIfAppRunningByName("NiceHashWorkerMonitor"))
+            {
+                Console.WriteLine("Error: Unable to update app due to NiceHashWorkerMonitor still running.");
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
+        }
+        private static bool CheckIfAppRunningByName(string name)
+        {
+            Process[] pname = Process.GetProcessesByName(name);
+            if (pname.Length == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private static void SaveInformationOnLatestRelease()
+        {
+            updaterFolder = Path.Combine(Directory.GetCurrentDirectory(), "Downloader");
+            if (!Directory.Exists(updaterFolder))
+            {
+                Directory.CreateDirectory(updaterFolder);
+            }
+            ReleaseInfo = DataHelper.GitHubReleases.GetRepoLatestReleaseNumber();
+            if (ReleaseInfo != null)
+            {
+                Console.WriteLine("Writing release information to file.");
+                File.WriteAllText(Path.Combine(updaterFolder, "LatestRelease.json"), ReleaseInfo.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Error: Unable to find information on latest release.");
+                Environment.Exit(1);
+            }
+        }
+        private static void DeleteFilesInCurrentDirectory()
+        {
+            System.IO.DirectoryInfo di = new DirectoryInfo(Environment.CurrentDirectory);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                if(!file.Name.Contains("NiceHashProfitabilityCalculatorUpdater") && !file.Name.Contains("LatestRelease.json") && !file.Name.Contains("Newtonsoft"))
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+        private static void DownloadAppDownloader()
+        {
+            int downloadCount = 0;
+            dynamic downloadedAsset = null;
+            foreach(dynamic asset in ReleaseInfo.assets)
+            {
+                if(asset.label == "Downloader")
+                {
+                    downloadCount++;
+                    DataHelper.DownloadHelper.DownLoadFileFromURL(asset.browser_download_url, asset.name);
+                    downloadedAsset = asset;
+                }
+            }
+            if(downloadCount != 1)
+            {
+                Console.WriteLine("Error: Found unexpected number of downloads for asset Downloader.");
+                Environment.Exit(1);
+            }
+            else
+            {
+                updaterFolder = Path.Combine(Directory.GetCurrentDirectory(), "Downloader");
+                Directory.CreateDirectory(updaterFolder);
+                DataHelper.DownloadHelper.ExtractZipFromFile(downloadedAsset.name.ToString(), updaterFolder);
+            }
+        }
+        private static void StartUpdateDownloader()
+        {
+            Process.Start(Path.Combine(updaterFolder, "NiceHashProfitabilityCalculatorDownloader.exe"));
+        }
+    }
+}
